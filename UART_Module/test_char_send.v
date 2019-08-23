e9215ac718de2ac0e2c2c853e6d7e76297d0b5f5
WayneGong <22903193+WayneGong@users.noreply.github.com>
2019-08-23 10:39:57 +0800

    Update test_char_send.v

--- UART_Module/test_char_send.v
+++ UART_Module/test_char_send.v
@@ -20,20 +20,6 @@ begin
 		time_cnt	<=	time_cnt	+	1'b1;
 end
 
-//wire	[7:0]	char2	=	send_str[23:16];		//15-8
-//wire	[7:0]	char1	=	send_str[15:8];		//15-8
-//wire	[7:0]	char0	=	send_str[7:0];		//7-0
-
-//	send_str	=	{x1_l,x1_r,x2_l,x2_r,y,x1,x2};
-//	send_str	total is 16bit
-//	x1_l,		1bit,send_str[15]		
-//	x1_r,		1bit,send_str[14]	
-//	x2_l,		1bit,send_str[13]	
-//	x2_r,		1bit,send_str[12]	
-//	y,			4bit,send_str[11:8]
-//	x1,			4bit,send_str[7:4]	
-//	x2			4bit,send_str[3:0]	
-	
 
 always@(*)
 begin
@@ -49,7 +35,7 @@ begin
 		tx_data_in	=	8'h00;		
 end
 
-always@(*)
+always@(*)			//每隔1秒，发送4个测试数据
 begin
 	if( time_cnt <=	3 )
 		tx_en	=	1'b1;
