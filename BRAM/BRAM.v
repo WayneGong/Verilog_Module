f0e53f8571607f6f3029aae66737dcdb61e07897
WayneGong <22903193+WayneGong@users.noreply.github.com>
2019-09-17 17:19:27 +0800

    更新BRAM文件

--- BRAM/BRAM.v
+++ BRAM/BRAM.v
@@ -1,20 +1,19 @@
 ////////////////////////////////////////////////////////////////////////////////
-// AHB-Lite Memory Module
+
 ////////////////////////////////////////////////////////////////////////////////
-module AHB2MEM
-   #(	parameter 	MEMWIDTH 	= 8,		//256 * data 
+module BRAM 
+   #(	parameter 	MEMWIDTH 	= 10,		//1024 * data 
 		parameter	DATAWIDTH 	= 32
-   )               // Size = 32KB
+   )               
    (
    input wire          	 			clk, 
-   input wire          	 			wen,
+   input wire          	 			wr_en,
+   input wire						rd_en,
    input wire	[MEMWIDTH-1:0]		waddr,		//写地址
    input wire	[MEMWIDTH-1:0]		raddr,		//读地址
    input wire	[DATAWIDTH-1:0] 	data_in,	//写数据
    output reg	[DATAWIDTH-1:0] 	data_out	//读数据
    );
-
-
 
 // Memory Array
 reg  [DATAWIDTH-1:0] memory[0:(2**(MEMWIDTH)-1)];
@@ -26,12 +25,13 @@ end
 
 always@(posedge clk)
 begin
-	if(wen)
+	if( wr_en )
 		memory[waddr]	<=	data_in;
 end   
 
 always@(posedge clk)
 begin
+	if( rd_en )
 	data_out	<=	memory[raddr];
 end
 
